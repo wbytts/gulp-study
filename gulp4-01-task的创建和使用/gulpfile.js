32f7b36f3985753b175d889e0c00655b9584da25
wangbingyi <1041493283@qq.com>
2022-05-01 03:21:27 +0800

    update

---
+++ gulp4-01-task的创建和使用/gulpfile.js
@@ -0,0 +1,42 @@
+﻿const gulp = require('gulp')
+
+// gulp 后不跟任何任务名称时，会执行 default
+// 也可以直接 gulp default
+gulp.task('default', function(callback) {
+  console.log('gulp is running ...');
+  callback();
+});
+
+
+// 将任务函数单独拿出去
+function t1(callback) {
+  console.log('task1 ....');
+  callback();
+}
+
+gulp.task('task1', t1);
+
+// 直接将任务函数传给 gulp.task，此时默认以函数名作为任务名
+function task2(callback) {
+  console.log('task2 ...');
+  callback();
+}
+
+gulp.task(task2);
+
+// 修改任务函数的 displayName 属性，可以改变注册的任务名
+function t3(callback) {
+  console.log('task3 ...');
+  callback();
+}
+
+t3.displayName = 'task3'
+gulp.task(t3);
+
+// 串行执行
+// gulp.series(t1, t2, t3) 多个任务按顺序串行执行
+/// 可以传任务名，也可以传任务函数
+gulp.task('all', gulp.series('task1', 'task2', 'task3'))
+
+// 并行执行 gulp.parallel
+gulp.task('async-all', gulp.parallel('task1', 'task2', 'task3'))
---
+++ gulp4-02-文件处理/gulpfile.js
@@ -0,0 +1,19 @@
+﻿const gulp = require('gulp');
+
+function copy1(cb) {
+  return gulp.src('src/assets/**/*.*') // node 一个流，pipe
+  .pipe(gulp.dest('dist/assets/')); // gulp.dest：把所有文件保存到指定位置
+}
+
+function copy(cb) {
+  return gulp.src([
+    'src/assets/**/*.*',
+    'src/lib/**/*.*',
+  ], {
+    base: 'src/' // 按照 src 目录的结构来处理输出的结构，如果不设置可能会把很多东西都堆在一起
+  })
+  .pipe(gulp.dest('dist/assets/'));
+}
+
+gulp.task(copy)
+
